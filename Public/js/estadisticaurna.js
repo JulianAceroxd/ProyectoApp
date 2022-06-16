cbbf4d03aefc565cb172a96a488ac0604718abf0
JulianAceroxd <92897637+JulianAceroxd@users.noreply.github.com>
2022-06-16 18:18:41 -0500

    hola

--- Public/js/estadisticaurna.js
+++ Public/js/estadisticaurna.js
@@ -4,13 +4,13 @@ $(document).ready(function(){
 
     let btn=$('.botons').index(this);
     let doc=$('.id').eq(btn);
-    const traerDatos= async ()=>{
+    
     let d=doc.val();
 
    const d2={
        id: d
    };
-  
+  alert(d2)
    let =url='/urnadatos'
    fetch(url,{
     method:'POST',
@@ -30,67 +30,14 @@ console.log("aca va el tamaño"+data.length)
 
 
    for(i=0; i<data.length;i++){
-    var button = '<form action="/Votos" method="post"><input type="submit"value="'+data[i].Opcion+'"  id="num_parrafos" name="opcion"/><input type="hidden"value="'+results.map(item=>item.IdUrna)[0]+'"  id="num_parrafos" name="id"/></form>'
-    $('#botones'+d2 ).append(button);
+    var button = '<form action="/Votos" method="post"><input type="submit"value="'+data[i].Opcion+'"  id="num_parrafos" name="opcion"/><input type="hidden"value="'+data[i].IdUrna+'"  id="num_parrafos" name="id"/></form>'
+    $('#botones'+d ).append(button);
     console.log('botones'+d)
     console.log("aca van los datos"+data[i].Opcion)      
 
 }}
   
     
-    const data=await fetch ('/urnadatos',{
-        method:'POST',
-        headers:{
-
-            "content-Type":"application/json",
-        },
-        body: JSON.stringify(d2),
-    }
     
-    );
- 
-    const results=await data.json();
-  
-    const finalData=[{
-       
-        x:results.map(item=>item.Opcion),
-        y:results.map(item=>{ 
-
-            const cantidadtotal=results.reduce((a,b)=>{
-      
-    
-       return  a+b.cantidad 
-            },0)
-            
-            const rt= item.cantidad/cantidadtotal;
-            console.log(cantidadtotal);
-            return rt*100;
-            
-            
-        }),
-        type:"bar"
-    }]
-   
-for( let i3=0; i3<results.map(item=>item.Opcion).length;i3++){
- 
-  
-   
-   
-
-}
-   const resultado = document.getElementById('resultados')
-  
-
-
-
-
-    Plotly.newPlot('plotlyChart'+d,finalData)
-
-     }
-     traerDatos();
-
-
-    
-
     });
 });
